19fe61ffa382309557415b304b7ec486fc7775ba
Jshelper307 <jshelper307@gmail.com>
2024-11-18 19:26:25 +0530

    fix contact.css

--- pages/contact.js
+++
@@ -1,7 +0,0 @@
-// Contact Form Submission
-document.getElementById('contactForm').addEventListener('submit', function (event) {
-    event.preventDefault();
-    alert('Thank you for reaching out! We will get back to you soon.');
-    this.reset();
-  });
-  
